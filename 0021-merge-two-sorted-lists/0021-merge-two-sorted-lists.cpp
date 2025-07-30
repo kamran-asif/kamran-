f420c89d999016fcfccd0bf354b344ad134a164a
kamran-asif <mdkamranasif789@gmail.com>
2025-07-30 22:00:30 +0530

    Time: 0 ms (100%), Space: 19.7 MB (11.37%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#include <queue>
+#include <vector>
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // comparator for min heap
+        auto cmp = [](ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        };
+        std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(cmp)> pq(cmp);
+
+        // heads ko heap me dalo
+        if (list1) pq.push(list1);
+        if (list2) pq.push(list2);
+
+        // dummy node for the result list
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        // jb tk heap empty nhi h
+        while (!pq.empty()) {
+            //smallest node uthao
+            ListNode* node = pq.top();
+            pq.pop();
+
+            // attach in result
+            tail->next = node;
+            tail = tail->next;
+            // if next node exist then try to push on heap
+            if (node->next) {
+                pq.push(node->next);
+            }
+        }
+
+        return dummy.next;
+    }
+};
