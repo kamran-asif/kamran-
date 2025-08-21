f616456d1b45ddf30539a0ef25bea2785112870e
kamran-asif <mdkamranasif789@gmail.com>
2025-08-21 17:44:59 +0530

    Update 0003-longest-substring-without-repeating-characters.cpp

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //taking a map size 256
+        //tking a map size 256
         vector<int>mp(256,-1);
         //2pointer
         int left=0,right=0;
@@ -9,13 +9,13 @@ public:
         int len=0;
         // iterating over map
         while(right<n){
-            //if the s[right] is seen then perform the task
+            //if the s[right] is seen then prform the tsk
             if(mp[s[right]]!=-1)
-            //update left max(s[right]+1,left)
+            //updte left max(s[right]+1,left)
                 left=max(mp[s[right]]+1,left);
-                //right into the map in position of s[right]
+                //right into the map in psitin of s[right]
                 mp[s[right]]=right;
-                //update len max(len,right-left+1)
+                //updte len max(len,right-left+1)
                 len=max(len,right-left+1);
                 //right ko bdhao
                 right++;
