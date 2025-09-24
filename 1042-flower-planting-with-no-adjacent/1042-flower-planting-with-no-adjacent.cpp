1439d3a4b9395b4ca467667ca313da11eff7ee64
kamran-asif <mdkamranasif789@gmail.com>
2025-09-24 19:40:28 +0530

    Time: 54 ms (45.32%), Space: 48.2 MB (68.8%) - LeetHub

---
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+        vector<vector<int>> adj(n);
+        for (auto &p : paths) {
+            int u = p[0] - 1;
+            int v = p[1] - 1;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> ans(n, 0);  
+        for (int i = 0; i < n; i++) {
+            vector<bool> used(5, false); 
+            for (int nei : adj[i]) {
+                if (ans[nei] != 0) {
+                    used[ans[nei]] = true;
+                }
+            }
+            for (int f = 1; f <= 4; f++) {
+                if (!used[f]) {
+                    ans[i] = f;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
