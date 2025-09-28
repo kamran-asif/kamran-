45a7ace7a6901e643631e38eb1248e10bfa7ff2a
kamran-asif <mdkamranasif789@gmail.com>
2025-09-29 02:35:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.cpp
+++ 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.cpp
@@ -3,6 +3,7 @@ public: // kafi acha sval h
     int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience){
         int n=patience.size();
         vector<vector<int>>graph(n);
+        //kch na adjncy lst bna re bs
         for (auto &e:edges){
             graph[e[0]].push_back(e[1]);
             graph[e[1]].push_back(e[0]);
@@ -25,16 +26,15 @@ public: // kafi acha sval h
             }
         }
 
-        // ntwrk idle k tme clclte krte
+        // ntwrk idle k tme clclte krte h
         long long ans=0;
         for (int i=1;i<n;i++){ 
             int rt=dist[i]*2; 
 
             // lst scnd srvr i ne msg bhja befre rply aya
-            int lstsnd=((rt-1)/patience[i])*patience[i];
-
-            // lst msg k rply srvr i pr ane k time
-            long long lstarrival=(long long)lstsnd+rt;
+            int lstsnd=((rt-1)/patience[i])*patience[i]
+             
+            long long lstarrival=(long long)lstsnd+rt; // lst msg k rply srvr i pr ane k time
 
             ans=max(ans,lstarrival); 
         }
