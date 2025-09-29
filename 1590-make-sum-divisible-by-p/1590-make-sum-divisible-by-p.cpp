5dd614c6829c353e61b48e313b018a13df66fbab
kamran-asif <mdkamranasif789@gmail.com>
2025-09-30 01:18:54 +0530

    Time: 91 ms (26.08%), Space: 87.7 MB (69.36%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public: //t.c=o(n) i guess s.c=o(min(n,p))
+    int minSubarray(vector<int>& nums, int p) {
+      long long ttlsum=0;
+      for(int x:nums)ttlsum+=x;
+      int remainder=ttlsum%p;
+      if(remainder==0)return 0;
+      unordered_map<int,int>lstindex;
+      lstindex[0]=-1; 
+      long long prefix=0;
+      int res=nums.size();
+      for(int i=0;i<nums.size();i++){
+        prefix=(prefix+nums[i])%p;
+        int trgt=(prefix-remainder+p)%p;
+        if(lstindex.count(trgt)){
+            res=min(res,i-lstindex[trgt]);
+        }
+        lstindex[prefix]=i;
+      }
+      return (res==nums.size())?-1:res;  
+    }  
+};
