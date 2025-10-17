4a2ace97c59801167ebc7b9c245b21a5c54fda8a
kamran-asif <mdkamranasif789@gmail.com>
2025-10-17 08:40:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.15%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.size();
+        if(n==0)return 0;
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=(s[0]=='0')?0:1;
+        for(int i=2;i<=n;i++){
+            if(s[i-1]!='0')dp[i]+=dp[i-1];
+            if(s[i-2]!='0'){
+                int two=(s[i-2]-'0')*10+(s[i-1]-'0');
+                if(two>=10&&two<=26)dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
