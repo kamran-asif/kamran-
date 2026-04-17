d81e4f9107c73cfb73b48ac6991e8322a1c63999
Kamran <mdkamranasif789@gmail.com>
2026-04-18 03:34:25 +0530

    Fix increment operation in string comparison logic

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -8,7 +8,7 @@ public:
 
         for (int i = 0; i < s1.length(); i++) {
             int offset = (i & 1) << 7;
-            counts[offset + s1[i]]++;
+        counts[offset + s1[i]]++;
             counts[offset + s2[i]]--;
         }
 
