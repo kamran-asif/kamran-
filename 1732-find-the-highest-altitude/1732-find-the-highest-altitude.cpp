368137fa115856a5467470558e01d0a263e38569
Kamran <mdkamranasif789@gmail.com>
2026-06-19 23:37:53 +0530

    Time: 0 ms (100%), Space: 10.8 MB (44.68%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -4,14 +4,14 @@ public:
         int currentAltitude = 0;
         // Highest altitude currently is 0.
         int highestPoint = currentAltitude;
-        
+
         for (int altitudeGain : gain) {
             // Adding the gain in altitude to the current altitude.
             currentAltitude += altitudeGain;
             // Update the highest altitude.
             highestPoint = max(highestPoint, currentAltitude);
         }
-        
+
         return highestPoint;
     }
 };
