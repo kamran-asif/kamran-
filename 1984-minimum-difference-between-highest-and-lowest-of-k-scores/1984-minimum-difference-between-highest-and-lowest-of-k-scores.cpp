4183b6633b33fb6f09d5a41f918270839f287db9
Kamran <mdkamranasif789@gmail.com>
2026-07-14 23:47:31 +0530

    Refactor minimumDifference function
    Refactor minimumDifference function to improve clarity.

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -3,7 +3,7 @@ class Solution{
 public:
 int minimumDifference(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int minDiff = nums[k-1] - nums[0];
+ int minDiff = nums[k-1] - nums[0];
         
         for (int i = 1; i <= nums.size() - k; i++) {
             int currentDiff = nums[i + k - 1] - nums[i];
