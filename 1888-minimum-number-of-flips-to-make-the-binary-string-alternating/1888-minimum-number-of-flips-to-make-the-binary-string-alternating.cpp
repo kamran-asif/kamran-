939c62b8ec60d1ad8bc41c433932fb1be5fb8bce
Kamran <mdkamranasif789@gmail.com>
2026-08-11 00:00:06 +0530

    Update 1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int minFlips(string s) {
-        // Characteristic function
-        auto I = [](char ch, int x) -> int { return ch - '0' == x; };
+        // Characteristic function        auto I = [](char ch, int x) -> int { return ch - '0' == x; };
         int n = s.size();
         vector<vector<int>> pre(n, vector<int>(2));
         // Note the boundary case when i=0
