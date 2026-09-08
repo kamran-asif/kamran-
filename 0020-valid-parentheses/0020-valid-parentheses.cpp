7b863108505c899c7e087ee013994fa07c9eae8a
Kamran <mdkamranasif789@gmail.com>
2026-09-08 13:33:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (88.95%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-bool isValid(string s) {
-    stack<char> st;
-
-        for(char c : s) {
-
-
-
-                // Opening bracket
-                if(c == '(' || c == '{' || c == '[') {
-                    st.push(c);
-                }
-
-                // Closing bracket
-                else {
-                    if(st.empty())
-                        return false;
-
-                    if(c == ')' && st.top() != '(')
-                        return false;
-
-                    if(c == '}' && st.top() != '{')
-                        return false;
-
-                    if(c == ']' && st.top() != '[')
-                        return false;
-
-                    st.pop();
-                }
-            }
+    bool isValid(string s) {
+    stack<char>st;
+    for(char c:s){
+        if(c=='('||c=='{'||c=='['){
+            st.push(c);
+        }else{
+            if(st.empty())
+            return false;
         
-
-        return st.empty();
     
+    char top=st.top();
+    st.pop();
+    if(c==')'&&top!='(')return false;
+    if(c=='}'&&top!='{')return false;
+    if(c==']'&&top!='[')return false; 
+   
+
     }
+    }
+    return st.empty();
+}
 };
